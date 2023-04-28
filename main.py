428e8caca4cc1a4e3d4226e53749421053cc28f6
Kain-F <80208910+Kain-F@users.noreply.github.com>
2023-04-28 15:34:48 +0200

    main.py aangemaakt + util.py opgeschoont zodat we enkel de nodige methodes hebben staan

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import util
+import datetime
 
+if __name__ == '__main__':
+	mandatendatabank = util.Mandaten()
+	query = mandatendatabank.query()
+	query.to_csv(f'mandatendatabank_{datetime.date.today()}.csv')
+	print(query.describe())
